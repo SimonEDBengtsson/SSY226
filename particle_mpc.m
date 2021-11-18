c138575114975e47637322847b5cdca693893115
Victor Hanson <vectoralarm@gmail.com>
2021-11-18 15:54:18 +0100

    cleaned up model and tweak mpc params

--- particle_mpc.m
+++ particle_mpc.m
@@ -29,8 +29,8 @@ mpc_controller = mpc(sys_d);
 mpc_controller.Weights.OutputVariables = [10 10 10];
 mpc_controller.Weights.ManipulatedVariables = [1 1 1];
 mpc_controller.Weights.ManipulatedVariablesRate = [1 1 1];
-mpc_controller.ControlHorizon = 5;
-mpc_controller.PredictionHorizon = 20;
+mpc_controller.ControlHorizon = 3;
+mpc_controller.PredictionHorizon = 10;
 
 mpc_controller
 %open('particle_model.slx');
@@ -40,25 +40,26 @@ mpc_controller
 y_actual = out.yout{1}.Values.Data;
 y_reference = out.yout{2}.Values.Data;
 t = out.tout;
-size = [1 1];
+size = [0.8 0.8];
+plot_axis = [-1 11 -2 2];
 clear frames;
 
-plot(1); clf; grid on; hold on; axis equal; axis([-3 3 -2 12]);
-
+plot(1); clf; grid on; hold on; axis equal; axis(plot_axis);
+xlabel('x [m]'); ylabel('y [m]');
 circ_actual = rectangle('position', [-size/2 size], 'curvature', [1 1], 'linewidth', 3);
-arrow_actual = quiver(0, 0, 1*sin(0), 1*cos(0), 'linewidth', 3);
+arrow_actual = quiver(0, 0, 1*cos(0), 1*sin(0), 'linewidth', 3);
 circ_reference = rectangle('position', [-size/2 size], 'curvature', [1 1], 'edgecolor', 'g');
-arrow_reference = quiver(0, 0, 1*sin(0), 1*cos(0), 'linewidth', 2);
+arrow_reference = quiver(0, 0, 1*cos(0), 1*sin(0), 'linewidth', 2);
 
 for x = 1:length(t)
     frames(x) = getframe;
     circ_actual.Position = [y_actual(x,1)-size(1)/2, y_actual(x,2)-size(2)/2, size];
     arrow_actual.XData = y_actual(x,1); arrow_actual.YData = y_actual(x,2);
-    arrow_actual.UData = sin(y_actual(x,3)); arrow_actual.VData = cos(y_actual(x,3));
+    arrow_actual.UData = cos(y_actual(x,3)); arrow_actual.VData = sin(y_actual(x,3));
     
     circ_reference.Position = [y_reference(x,1)-size(1)/2, y_reference(x,2)-size(2)/2, size];
     arrow_reference.XData = y_reference(x,1); arrow_reference.YData = y_reference(x,2);
-    arrow_reference.UData = sin(y_reference(x,3)); arrow_reference.VData = cos(y_reference(x,3));
+    arrow_reference.UData = cos(y_reference(x,3)); arrow_reference.VData = sin(y_reference(x,3));
 end
 %%
 
