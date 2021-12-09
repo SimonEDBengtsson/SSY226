380c2d8c39217aece9d4a9f310d8ea8ef2bca8dd
Simon Bengtsson <simb@student.chalmers.se>
2021-12-09 16:01:56 +0100

    Fixed global naming bug in reference generator

--- reference_generator.m
+++ reference_generator.m
@@ -4,14 +4,14 @@ r_0=[0
      v_0
      0
      0];
- radius=100;
- angle=80;
+ turn_radius=100;
+ turn_angle=80;
  l_initial=400;
  nTurns=6;
  l_end=50;
  v_norm=@(t)10;
  
- l=l_initial+nTurns*deg2rad(angle)*radius+l_end;
+ l=l_initial+nTurns*deg2rad(turn_angle)*turn_radius+l_end;
  super_sample_ratio=100;
  T_gen=T/super_sample_ratio;
  acc=0;
@@ -23,19 +23,21 @@ r_0=[0
  t=0:T_gen:t_end;
  R=zeros(length(r_0),length(t));
  R(:,1)=r_0;
+ acc=0;
  for i=2:length(t)
      x=R(1,i-1); psi=R(3,i-1); v=v_norm(t(i-1));
      vx=v*cos(psi);
      vy=v*sin(psi);
-     if x<l_initial
+     if acc<l_initial
          psi_dot=0;
-     elseif (x-l_initial)/sin(deg2rad(angle))/radius<nTurns
-         psi_dot=-v/radius*(mod(fix((x-l_initial)/sin(deg2rad(angle))/radius),2)*2-1);
+     elseif (acc-l_initial)/deg2rad(turn_angle)/turn_radius<nTurns
+         psi_dot=-v/turn_radius*(mod(fix((acc-l_initial)/deg2rad(turn_angle)/turn_radius),2)*2-1);
      else
          psi_dot=0;
      end
      R(1:3,i)=R(1:3,i-1)+T_gen*[vx;vy;psi_dot];
      R(4:6,i)=[vx;vy;psi_dot];
+     acc=acc+vecnorm(R(1:2,i)-R(1:2,i-1));
  end
  t=t(1:super_sample_ratio:end);
  R=R(:,1:super_sample_ratio:end);
