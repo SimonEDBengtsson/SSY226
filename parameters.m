25c44774c616181d6f21c6d8d2bf8785bf7309c3
Simon Bengtsson <simb@student.chalmers.se>
2021-12-16 16:12:22 +0100

    Added support for different drive and steering architectures

--- parameters.m
+++ parameters.m
@@ -1,41 +1,21 @@
 clc; clear;
+
+% Parameters
 front_length=1.4;
 rear_length=1.6;
 width=2;
 height=0.35;
-% Full rigid-body hub-force to particle forces matrix
-r_1=[front_length;width/2;-height];
-r_2=[front_length;-width/2;-height];
-r_3=[-rear_length;width/2;-height];
-r_4=[-rear_length;-width/2;-height];
-i_hat=[1;0;0]; j_hat=[0;1;0];
-r=[r_1 r_2 r_3 r_4];
-
-B=[repmat(eye(3),[1 4])
-   cross(repmat(r_1,[1 3]),eye(3),1)...
-   cross(repmat(r_2,[1 3]),eye(3),1)...
-   cross(repmat(r_3,[1 3]),eye(3),1)...
-   cross(repmat(r_4,[1 3]),eye(3),1)];
-% Front-wheel steering
-% Forces in x and y, as well as yawing torque
-iOutputForces=[1 2 6];
-% x and y on front wheels, only x on back wheels
-iInputForces=[1 2 4 5 7 8 10 11];
-B_r=B(iOutputForces,iInputForces);
-% Force allocation matrix
-A=zeros(size(B'));
-A(iInputForces,iOutputForces)=pinv(B_r);
-
-% Parameters
 radius=0.4;
 mass=2000;
 I=4000*eye(3);
-mu=1;
+mu=0.6;
 turn_limit=deg2rad(30);
 v_0=20;
 v_norm=@(t)v_0;
 Q_ae=eye(3);
 Q_tau=eye(4);
+torque_distribution=[1 1 0 0];
+steerable=[1 1 0 0];
 
 % MPC
 T_c=0.1; % Controller period
@@ -50,15 +30,25 @@ sysc=ss(Ac,Bc,[],[]);
 sysd=c2d(sysc,T_d);
 Ad=sysd.A; Bd=sysd.B;
 reference_generator;
-% tau=0:T:1000;
-% r=@(t)[1000*cos(t/100-pi/2)
-%       -1000*(sin(t/100-pi/2)+1)
-%       -t/100
-%       -10*sin(t/100-pi/2)
-%       -10*cos(t/100-pi/2)
-%       -1/100];
-% R=cell2mat(arrayfun(r,tau,'UniformOutput',false));
 
+% Full rigid-body hub-force to particle forces matrix
+r_1=[front_length;width/2;-height];
+r_2=[front_length;-width/2;-height];
+r_3=[-rear_length;width/2;-height];
+r_4=[-rear_length;-width/2;-height];
+i_hat=[1;0;0]; j_hat=[0;1;0];
+r=[r_1 r_2 r_3 r_4];
+% Control allocator
+B=[repmat(eye(3),[1 4])
+   cross(repmat(r_1,[1 3]),eye(3),1)...
+   cross(repmat(r_2,[1 3]),eye(3),1)...
+   cross(repmat(r_3,[1 3]),eye(3),1)...
+   cross(repmat(r_4,[1 3]),eye(3),1)];
+% Forces in x and y, as well as yawing torque
+iOutputForces=[1 2 6];
+% No z forces
+iInputForces=[1 2 4 5 7 8 10 11];
+B_r=B(iOutputForces,iInputForces);
 %% Wheel direction controller H_inf
 tau=1;
 G=1/tf('s');% Internal plant model
