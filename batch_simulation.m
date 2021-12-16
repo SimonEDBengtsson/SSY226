c916c2098810b6f50f190b9a2c4e50495b76010a
Victor Hanson <vectoralarm@gmail.com>
2021-12-16 20:29:42 +0100

    batch simulation w/ arch and mu

--- batch_simulation.m
+++ batch_simulation.m
@@ -1,17 +1,25 @@
-mu_set = [0.8, 0.50];
+mu_set = [0.60, 0.575, 0.55];
+arch_type = ["AWD, Full individual control", "FWD, 50/50 torque split"];
+arch_torque = [1 2 3 4; 1 1 0 0];
+arch_steer = [1 1 1 1; 1 1 1 1];
 n = length(mu_set);
-close all;
+m = length(arch_type);
 
-for i=(1:n)
-    subplot(round(n/2), 2, i);
-    mu = mu_set(i);
-    sim('closed_loop');
-    % Prepare output for simulation_saver
-    out = ans;
-    simulation_saver;
-    plot(states(:,1), states(:,2));
-    title('\mu=', mu_set(i));
-    hold on;
-    plot(reference(:,1), reference(:,2), '--');
-    axis equal;
+close all;
+for L=(1:m)
+    torque_distribution = arch_torque(L,:);
+    steerable = arch_steer(L,:);
+    for i=(1:n)
+        figure((L-1)*n+i); axis equal; 
+        mu = mu_set(i);
+        sim('closed_loop');
+        % Prepare output for simulation_saver
+        out = ans;
+        name = [arch_type{L}(1:3), '_', num2str(mu_set(i))];
+        simulation_saver;
+        plot(states(:,1), states(:,2));
+        hold on;
+        plot(reference(:,1), reference(:,2), '--');
+        title(arch_type(L), ['\mu=', num2str(mu_set(i))]);
+    end
 end
