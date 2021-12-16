ab6ce7401d806a691ac07b98856462fe4f7a6ea5
Victor Hanson <vectoralarm@gmail.com>
2021-12-16 16:22:25 +0100

    modified reference to match test cases

--- reference_generator.m
+++ reference_generator.m
@@ -6,9 +6,9 @@ r_0=[0
      0];
  turn_radius=50;
  turn_angle=80;
- l_initial=400;
+ l_initial=100;
  nTurns=6;
- l_end=50;
+ l_end=100;
  
  l=l_initial+nTurns*deg2rad(turn_angle)*turn_radius+l_end;
  super_sample_ratio=100;
